df0e7405d73fc8b283c722c0c608197b3e75053b
Jonathan Woo <jonathan.ck.woo@gmail.com>
2022-01-31 00:40:03 -0500

    Update plotter.py
    Script to generate plots of intensity vs sensor angle

--- Black Body Radiation/plotter.py
+++ Black Body Radiation/plotter.py
@@ -1,1 +1,29 @@
-data=open("BlackbodyRadiationData1/30-4V-0.372A-g10_Trial1.txt")
+import matplotlib.pyplot as plt
+import os
+
+dir="/Users/jwoo/Documents/GitHub/PHY294 Labs/Black Body Radiation/BlackbodyRadiationData1/"
+files=os.listdir(dir)
+
+for i in range(len(files)):
+    if(not files[i].endswith(".txt")):
+        continue
+    file=open(dir + files[i], "r", 2)
+    data=file.read().split("\n")
+    data=data[2:]
+
+    theta=[]
+    volt=[]
+
+    for e in range(len(data)):
+        data[e]=data[e].split("\t")
+        volt.append(float(data[e][1]))
+        theta.append(float(data[e][0]))
+
+    plt.figure()
+    plt.plot(theta,volt)
+    plt.title(files[i][:-4])
+    plt.xlabel("Sensor Position (Theta)")
+    plt.ylabel("Light Intensity (Volts)")
+    plt.savefig(dir+"plots/"+files[i][:-4]+".png")
+
+plt.show()
